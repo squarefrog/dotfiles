6d27da21e59773055d20daabfed3e97e3a7c7dc4
Paul Williamson <squarefrog@gmail.com>
2017-08-15 22:49:31 +0100

    Reset prompt colours better

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -12,16 +12,16 @@ precmd () {
   STATUS=$(command git status --porcelain 2> /dev/null | tail -n1)
 
   if [[ -n $STATUS ]]; then
-    local git_branch='%F{red}$vcs_info_msg_0_%{$reset_color%}'
+    local git_branch='%F{red}$vcs_info_msg_0_%F{none}'
   else
-    local git_branch='%F{green}$vcs_info_msg_0_%{$reset_color%}'
+    local git_branch='%F{green}$vcs_info_msg_0_%F{none}'
   fi
 
   RPROMPT="${git_branch}"
 
   if [ "$ZSH_CLEAN_PATH_STYLE" = "1" ]; then
-    PROMPT="%F{blue}%c %F{white}"
+    PROMPT="%F{blue}%c %F{none}"
   else
-    PROMPT="%F{blue}%~ %F{white}"
+    PROMPT="%F{blue}%~ %F{none}"
   fi
 }
