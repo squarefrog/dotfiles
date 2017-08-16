a63be92da0137747048e4363e0d771989c5dc0e4
Paul Williamson <squarefrog@gmail.com>
2017-08-16 14:12:47 +0100

    Add options file

---
+++ zsh/.zsh/options.zsh
@@ -0,0 +1,9 @@
+# Allow files beginning with a . to be matched without specifying the dot
+setopt globdots
+
+# Change directory by simply typing the name
+setopt autocdo
+
+# Correct spelling mistakes. Comment out to disable.
+setopt correct
+
