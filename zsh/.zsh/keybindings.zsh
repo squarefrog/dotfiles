616444d3611565d9864435873c48859e709fc571
Paul Williamson <squarefrog@gmail.com>
2017-08-19 10:41:43 +0100

    Fix backspace in vi mode

--- zsh/.zsh/keybindings.zsh
+++ zsh/.zsh/keybindings.zsh
@@ -2,3 +2,8 @@ bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
+
+bindkey "^?" backward-delete-char # Delete beyond insert point in vi mode
+bindkey "^W" backward-kill-word
+bindkey "^H" backward-delete-char      # Control-h also deletes the previous char
+bindkey "^U" backward-kill-line
