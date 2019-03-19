a21830658401855ea27c6a21710baf46def69159
Paul Williamson <squarefrog@gmail.com>
2019-03-19 09:53:22 +0000

    Fix colours

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -16,6 +16,7 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 # OS X context colouring
 export CLICOLOR=1
 export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
+export TERM=xterm-256color
 
 # Path to dotfiles repo
 export DOTFILES="$(dirname $(readlink $HOME/.vimrc))"
